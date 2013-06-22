2e5c422e5aa8fcbd42a67446f361d034ec32f479
Wolfram Hempel <hempel.wolfram@gmail.com>
2013-06-22 07:28:19 +0100

    Restructuring

--- js/ControlViewModel.js
+++ js/ControlViewModel.js
@@ -4,8 +4,10 @@ ControlViewModel = function()
 	this._pChartContainers = [];
 	this._pSparkLines = [];
 	this._nTimeout = null;
-	window.onresize = this._updateChartNumber.bind( this );
 
+	/**
+	* Renderers
+	*/
 	this.renderer = ko.observableArray([
 		{ "className": "CanvasApiRenderer", "name": "Canvas API Renderer" },
 		{ "className": "CanvasPixelRenderer", "name": "Canvas Pixel Renderer" }
@@ -17,18 +19,23 @@ ControlViewModel = function()
 	this.isRunning = ko.observable( true );
 	this.toggleText = ko.observable( "running" );
 	this.activeRenderer = ko.observable( this.renderer()[0].className );
-	this.activeRenderer.subscribe( this._applyRenderer.bind( this ) );
 	this.chartNumber = ko.observable( 1 );
-	this.chartNumber.subscribe( this._updateChartNumber.bind( this ) );
 	this.updatesPerSeconds = ko.observable( 1 );
-	this.updatesPerSeconds.subscribe( this.onFrequencyUpdate.bind( this ) );
 	this.value = ko.observable( 1 );
+
+	/**
+	* Events
+	*/
+	this.activeRenderer.subscribe( this._applyRenderer.bind( this ) );
+	this.chartNumber.subscribe( this._updateCharts.bind( this ) );
+	this.updatesPerSeconds.subscribe( this._onFrequencyUpdate.bind( this ) );
+	window.onresize = this._updateCharts.bind( this );
 
 	/**
 	* Startup
 	*/
-	this._updateChartNumber();
-	this.addPoint();
+	this._updateCharts();
+	this._addPoint();
 };
 
 ControlViewModel.prototype.setChartNumber = function( nOffset )
@@ -41,16 +48,47 @@ ControlViewModel.prototype.setChartNumber = function( nOffset )
 	}
 };
 
-ControlViewModel.prototype.onFrequencyUpdate = function()
+ControlViewModel.prototype.toggle = function()
 {
 	if( this.isRunning() )
 	{
 		clearTimeout( this._nTimeout );
-		this.addPoint();
+		this.isRunning( false );
+		this.toggleText( "paused" );
+	}
+	else
+	{
+		this._addPoint();
+		this.isRunning( true );
+		this.toggleText( "running" );
 	}
 };
 
-ControlViewModel.prototype._updateChartNumber = function()
+/************************************************************
+* PRIVATE METHODS											*
+************************************************************/
+ControlViewModel.prototype._addPoint = function()
+{
+	this.value( this.value() * (  0.999 + ( Math.random() / 500 ) ) );
+
+	for( var i = 0; i < this._pSparkLines.length; i++ )
+	{
+		this._pSparkLines[ i ].addDataPoint( this.value() );
+	}
+
+	this._nTimeout = setTimeout( this._addPoint.bind( this ), ( 1 / this.updatesPerSeconds() ) * 1000 );
+};
+
+ControlViewModel.prototype._onFrequencyUpdate = function()
+{
+	if( this.isRunning() )
+	{
+		clearTimeout( this._nTimeout );
+		this._addPoint();
+	}
+};
+
+ControlViewModel.prototype._updateCharts = function()
 {
 	var i, mData,
 	nChartContainerHeight = Math.floor( this._eContainer.offsetHeight / this.chartNumber() ) -1,
@@ -102,32 +140,4 @@ ControlViewModel.prototype._applyRenderer = function()
 	{
 		this._pSparkLines[ i ].setRenderer( new renderer[ this.activeRenderer() ]() );
 	}
-};
-
-ControlViewModel.prototype.toggle = function()
-{
-	if( this.isRunning() )
-	{
-		clearTimeout( this._nTimeout );
-		this.isRunning( false );
-		this.toggleText( "paused" );
-	}
-	else
-	{
-		this.addPoint();
-		this.isRunning( true );
-		this.toggleText( "running" );
-	}
-};
-
-ControlViewModel.prototype.addPoint = function()
-{
-	this.value( this.value() * (  0.999 + ( Math.random() / 500 ) ) );
-
-	for( var i = 0; i < this._pSparkLines.length; i++ )
-	{
-		this._pSparkLines[ i ].addDataPoint( this.value() );
-	}
-
-	this._nTimeout = setTimeout( this.addPoint.bind( this ), ( 1 / this.updatesPerSeconds() ) * 1000 );
 };
